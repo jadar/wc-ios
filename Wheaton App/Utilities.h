1b82ce36ad05916a1c250e5b54131df9a9af6a16
Chris Anderson <hi@chrisanderson.io>
2014-09-23 16:50:31 -0500

    iOS8 fixes

---
+++ Wheaton App/Utilities.h
@@ -0,0 +1,13 @@
+//
+//  Utilities.h
+//  Wheaton App
+//
+//  Created by Chris Anderson on 9/23/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Utilities : NSObject
+
+@end
---
+++ Wheaton App/Utilities.m
@@ -0,0 +1,13 @@
+//
+//  Utilities.m
+//  Wheaton App
+//
+//  Created by Chris Anderson on 9/23/14.
+//
+//
+
+#import "Utilities.h"
+
+@implementation Utilities
+
+@end
